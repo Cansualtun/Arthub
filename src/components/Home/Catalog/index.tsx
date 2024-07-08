529824dcb48ef93b99a9324ce7302850acaeb89c
Cansu Altun <cansualtun@192.168.1.96>
2024-07-09 00:25:01 +0300

    api bind

--- src/components/Home/Catalog/index.tsx
+++ src/components/Home/Catalog/index.tsx
@@ -1,69 +1,26 @@
-"use client";
 import Line from "@/components/Shared/Line";
-import { useAppDispatch, useAppSelector } from "@/hooks";
-import { getProductList } from "@/store/state/ProductSlice/thunk";
-import { useEffect } from "react";
+import PocketBase from 'pocketbase';
 
-export default function Catalog() {
-  const dispatch = useAppDispatch();
-  const product = useAppSelector((state) => state.product.getProductListState);
+export default async function Catalog() {
+  const pb = new PocketBase('https://api.pixem.org');
+  const records = await pb.collection('ozkamci_katalog').getFullList({
+    sort: '-created',
+  });
 
-  useEffect(() => {
-    dispatch(getProductList());
-  }, []);
-  console.log(product.data?.items , "cvans")
   return (
     <div className="flex flex-col gap-4 font-bold">
       <Line title="Katalog" />
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className="text-center">
-          <img
-            src="assets/images/alfa.svg"
-            alt="Photo 1"
-            className="w-full h-auto"
-          />
-          <p>Fotoğraf 1</p>
-        </div>
-        <div className="text-center">
-          <img
-            src="assets/images/alfa.svg"
-            alt="Photo 2"
-            className="w-full h-auto"
-          />
-          <p>Fotoğraf 2</p>
-        </div>
-        <div className="text-center">
-          <img
-            src="assets/images/alfa.svg"
-            alt="Photo 3"
-            className="w-full h-auto"
-          />
-          <p>Fotoğraf 3</p>
-        </div>
-        <div className="text-center">
-          <img
-            src="assets/images/alfa.svg"
-            alt="Photo 4"
-            className="w-full h-auto"
-          />
-          <p>Fotoğraf 4</p>
-        </div>
-        <div className="text-center">
-          <img
-            src="assets/images/alfa.svg"
-            alt="Photo 5"
-            className="w-full h-auto"
-          />
-          <p>Fotoğraf 5</p>
-        </div>
-        <div className="text-center">
-          <img
-            src="assets/images/alfa.svg"
-            alt="Photo 6"
-            className="w-full h-auto"
-          />
-          <p>Fotoğraf 6</p>
-        </div>
+        {records.map((record) => (
+          <div key={record.id} className="text-center">
+            <img
+              src={`https://api.pixem.org/api/files/${record.collectionId}/${record.id}/${record.image}`}
+              alt={record.name}
+              className="w-full h-auto"
+            />
+            <p>{record.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
