6d31f13ab25f317a36c70844dcc574a9d5702152
Cansu Altun <cansualtun@192.168.1.65>
2024-07-16 00:51:44 +0300

    fix about design

--- src/app/sepetim/page.tsx
+++ src/app/sepetim/page.tsx
@@ -1,16 +1,15 @@
 import CartList from "@/components/Cart/CartList";
 import ContactForm from "@/components/Cart/Contact";
-import Head from "next/head";
 
 export default function Sepetim() {
   return (
     <div>
       <div className="h-28 bg-primary-main">
-        <p className="text-3xl flex items-center justify-center h-full">
+        <p className="text-3xl flex items-center justify-center h-full w-[100%]">
           SEPETİM
         </p>
       </div>
-      <div className="max-w-[1100px] mx-auto mt-28 flex flex-row gap-40">
+      <div className="max-w-[1100px] mx-auto mt-28 flex flex-col  md:flex-row gap-8 md:gap-40">
         <ContactForm />
         <CartList />
       </div>
