3f4b97333b0d764baaa00a089ea4a01dd2853f68
Cansu Altun <cansualtun@Cansu-MacBook-Air.local>
2024-07-16 11:40:15 +0300

    build log fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,16 @@
-"use client";
-import About from "@/components/Home/About";
-import Catalog from "@/components/Home/Catalog";
-import Contact from "@/components/Home/Contact";
+import Home from "@/components/Home/Home";
 import { Suspense } from "react";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import BannerSlider from "@/components/Shared/Banner";
-import banners from "@/data";
 
-export default function Home() {
-  const searchParams = useSearchParams();
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-  useEffect(() => {
-    const scrollToContact = searchParams.get("scrollToContact");
-    if (scrollToContact) {
-      document.querySelector("#contact-section")?.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  }, [searchParams]);
-
+export default function Page({ searchParams }: Props) {
+  const scrollToContact = searchParams.scrollToContact === "true";
+  
   return (
-    <div>
-        <BannerSlider banners={banners} />
-        <About />
-        <div className="max-w-[1100px] mx-auto space-y-20">
-          <Catalog />
-          <div id="contact-section">
-            <Contact />
-          </div>
-        </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Home scrollToContact={scrollToContact} />
+    </Suspense>
   );
 }
